21f4e30b8820e37b7d43b5e11ca736f65c2d906e
jakobgriogoryan <hakob.grigoryan1992@gmail.com>
2019-08-12 17:38:06 +0400

    Profile model

---
+++ database/migrations/2019_08_12_131343_create_profiles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('avatar');
+            $table->string('phone_number');
+            $table->text('description');
+            $table->string('country');
+            $table->string('postal_code');
+            $table->string('industry');
+            $table->string('job_title');
+            $table->string('organization');
+            $table->foreign('user_id')->references('id')
+              ->on('users')->onDelete('cascade')->onUpdate('restrict');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
---
+++ resources/views/profile.blade.php
@@ -0,0 +1,154 @@
+
+<!-- Section 1 start -->
+
+<section class="site_section_wrapper bg-light">
+  <div class="container">
+	 
+
+	 
+	<div class="row">
+		<div class="col-lg-12"><h1 class="site_page_title">Profile</h1>  </div>  
+	</div>  
+	<hr class="mt-2 mb-5">  
+
+	<div class="row">
+	  
+		<div class="col-lg-6 mb-4">
+			<div class="row">
+				<div class="col-lg-12"><h4 class="site_select_category_title mb-0">Profile Photo</h4> </div>  
+			</div>  	
+			<hr class="mb-4">
+			
+			<ul class="site_select_photo_list list-inline">
+				<li class="list-inline-item"><img src="../vendor/images/profile-photo.png" class="site_your_profile_thumb img-fluid" alt=""></li>
+				<li class="list-inline-item">
+					<div class="site_input_file btn btn-lg btn-primary">
+						Select Profile Photo
+						<input type="file" name="file">
+					</div>
+				</li>
+			</ul>
+			
+			<div class="row">
+				<div class="col-lg-12"><h4 class="site_select_category_title mb-0">Basic Information</h4> </div>  
+			</div>  	
+			<hr class="mb-4">
+			
+			<form>
+				
+			<div class="site_profile_box">
+				
+			  <div class="form-group row">
+				<label class="col-sm-3 col-form-label">First Name</label>
+				<div class="col-sm-9">
+				  <input type="email" class="form-control" id="" placeholder="Chad">
+				</div>
+			  </div>
+				
+			  <div class="form-group row">
+				<label class="col-sm-3 col-form-label">Last Name</label>
+				<div class="col-sm-9">
+				  <input type="text" class="form-control" id="" placeholder="Stewart">
+				</div>
+			  </div>
+
+			  <div class="form-group row">
+				<label class="col-sm-3 col-form-label pt-0">Phone Number <small class="text-danger">Verify phone</small></label>
+				  
+				<div class="col-sm-9">
+				  <select class="form-control" id="">
+					  <option value="0">(913)887-2532</option>
+					  <option value="1">(913)887-2532</option>
+					  <option value="2">(913)887-2532</option>
+					  <option value="3">(913)887-2532</option>
+  				  </select>	  
+				</div>
+			  </div>
+
+			  <div class="form-group row mb-5">
+				<label class="col-sm-3 col-form-label d-flex"><span class="my-auto">Description</span></label>
+				<div class="col-sm-9">
+					<textarea class="form-control" id="" placeholder="Pellentesque habitant morbi tristique senectus et netus
+et malesuada fames ac turpis egestas. Ut arcu libero, nar non massa sed, accumsan scelerisque dui. Morbi purus mauris, vulputate quis felis nec, fermentum aliquam orci. Quisque ornare iaculis placerat." rows="8"></textarea>
+				</div>
+			  </div>
+
+			  <div class="form-group row">
+				<label class="col-sm-3 col-form-label"></label>
+				<div class="col-sm-9">
+					<button class="btn btn-primary site_btn px-5">Saved</button> 
+				</div>
+			  </div>
+				</div>	
+				
+			</form>	
+			
+		</div>
+		
+		
+		<div class="col-lg-6">
+			<div class="row">
+				<div class="col-lg-12 mb-4"><h1 class="site_detail_title">Details</h1></div>  
+				<div class="col-lg-12"><h4 class="site_select_category_title mb-0">Address</h4> </div>  
+			</div>  	
+			<hr class="mb-4">
+			
+			<form>
+			<div class="site_profile_box">
+				
+			  <div class="form-group row">
+				<label class="col-sm-3 col-form-label">Country</label>
+				<div class="col-sm-9">
+				  <input type="email" class="form-control" id="" placeholder="United States of America (USA)">
+				</div>
+			  </div>
+				
+			  <div class="form-group row mb-5">
+				<label class="col-sm-3 col-form-label">Zip/Postal Code</label>
+				<div class="col-sm-9">
+				  <input type="text" class="form-control" id="" placeholder="452221">
+				</div>
+			  </div>
+
+			<div class="row">
+				<div class="col-lg-12"><h4 class="site_select_category_title mb-0">Company Informantion</h4> </div>  
+			</div>  	
+			<hr class="mb-4">
+				
+			  <div class="form-group row">
+				<label class="col-sm-3 col-form-label">Industry</label>
+				<div class="col-sm-9">
+				  <input type="email" class="form-control" id="" placeholder="Enter">
+				</div>
+			  </div>
+				
+			  <div class="form-group row mb-5">
+				<label class="col-sm-3 col-form-label">Job Title</label>
+				<div class="col-sm-9">
+				  <input type="text" class="form-control" id="" placeholder="452221">
+				</div>
+			  </div>
+
+			  <div class="form-group row mb-5">
+				<label class="col-sm-3 col-form-label">Organization</label>
+				<div class="col-sm-9">
+				  <input type="text" class="form-control" id="" placeholder="452221">
+				</div>
+			  </div>
+				
+
+			  <div class="form-group row">
+				<label class="col-sm-3 col-form-label"></label>
+				<div class="col-sm-9">
+					<button class="btn btn-primary site_btn px-5">Saved</button> 
+				</div>
+			  </div>
+				</div>	
+				
+			</form>
+		</div>
+	</div> 
+  </div>
+</section>
+
+<!-- Section 1 end --> 
